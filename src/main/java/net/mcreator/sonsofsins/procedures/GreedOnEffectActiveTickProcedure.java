13899536a6ac4de3201dc192cc701b88eedde643
Naz <bluspringindohq@gmail.com>
2023-09-18 01:55:26 +0800

    let's *not* provide a way to kill everything please

--- src/main/java/net/mcreator/sonsofsins/procedures/GreedOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/sonsofsins/procedures/GreedOnEffectActiveTickProcedure.java
@@ -13,9 +13,7 @@ package net.mcreator.sonsofsins.procedures;
 
 import net.mcreator.sonsofsins.init.SonsOfSinsModParticleTypes;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.LevelAccessor;
 
@@ -25,10 +23,8 @@ public class GreedOnEffectActiveTickProcedure {
             return;
         }
         Entity _ent = entity;
-        _ent.teleportTo(entity.getX(), entity.getY() - 0.01, entity.getZ());
-        if (_ent instanceof ServerPlayer _serverPlayer) {
-            _serverPlayer.connection.teleport(entity.getX(), entity.getY() - 0.01, entity.getZ(), _ent.getYRot(), _ent.getXRot());
-        }
+        _ent.teleportTo(entity.getX(), entity.getY(), entity.getZ());
+
         if (world instanceof ServerLevel _level) {
             _level.sendParticles((ParticleOptions) SonsOfSinsModParticleTypes.SIN_BLOOD.get(), entity.getX(), entity.getY(), entity.getZ(), 2, 0.3, 0.3, 0.3, 0.01);
         }
